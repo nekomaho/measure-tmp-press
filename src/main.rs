f90e2da065aa3c38e26f644649ad098517dd5af6
nekomaho <nekosatoru@gmail.com>
2025-03-01 17:08:29 +0900

    Add bme280 setting

--- src/main.rs
+++ src/main.rs
@@ -5,11 +5,14 @@
 #![no_std]
 #![no_main]
 
-use defmt::*;
+use core::fmt::Write;
+
+use defmt::info;
 use embassy_executor::Spawner;
 use embassy_rp::gpio;
 use embassy_rp::i2c::{self, Config};
 use embassy_time::Timer;
+use embassy_time::Delay;
 use gpio::{Level, Output};
 use embedded_graphics::{
     mono_font::{ascii::FONT_6X10, MonoTextStyleBuilder},
@@ -17,8 +20,12 @@ use embedded_graphics::{
     prelude::*,
     text::{Baseline, Text},
 };
+use embedded_hal_1;
 use ssd1306::{prelude::*, I2CDisplayInterface, Ssd1306};
+use bme280::i2c::BME280;
 use {defmt_rtt as _, panic_probe as _};
+
+use heapless::String;
 
 // Program metadata for `picotool info`.
 // This isn't needed, but it's recomended to have these minimal entries.
@@ -33,11 +40,43 @@ pub static PICOTOOL_ENTRIES: [embassy_rp::binary_info::EntryAddr; 4] = [
     embassy_rp::binary_info::rp_program_build_attribute!(),
 ];
 
+fn f32_to_string(value: f32) -> String<16> {
+    let mut buf = String::<16>::new(); // for buffer
+    write!(&mut buf, "{:.2}", value).unwrap(); // format by under two digit of the decimal point
+    buf
+}
+
+fn get_bme280_result<I2C>(bme280: &mut BME280<I2C>) -> String<128>
+where
+    I2C: embedded_hal_1::i2c::ErrorType, I2C: embedded_hal_1::i2c::I2c // measure ofr BME280 requires this setting
+{
+    let measurements = bme280.measure(&mut Delay).unwrap();
+    let temperature: String<16>= f32_to_string(measurements.temperature);
+    let humidity: String<16>= f32_to_string(measurements.humidity);
+    let pressure: String<16> = f32_to_string(measurements.pressure/100.0);
+
+    let mut result: String<128> = String::new(); // for display result as the String
+
+    write!(&mut result, "{}{}\n", "t:", temperature.as_str()).unwrap();
+    write!(&mut result, "{}{}\n", "h:", humidity.as_str()).unwrap();
+    write!(&mut result, "{}{}", "p:", pressure.as_str()).unwrap();
+    result
+}
+
 #[embassy_executor::main]
 async fn main(_spawner: Spawner) {
     let p = embassy_rp::init(Default::default());
     let mut led = Output::new(p.PIN_25, Level::Low);
 
+    // For BME280
+    let sda_bme280 = p.PIN_0;
+    let scl_bme280 = p.PIN_1;
+
+    let i2c_bme280 = i2c::I2c::new_blocking(p.I2C0, scl_bme280, sda_bme280, Config::default());
+    let mut bme280 = BME280::new_primary(i2c_bme280);
+    bme280.init(&mut Delay).unwrap();
+
+    // For OLED
     let sda = p.PIN_2;
     let scl= p.PIN_3;
 
@@ -57,17 +96,18 @@ async fn main(_spawner: Spawner) {
 
     loop {
         info!("led on!");
-        Text::with_baseline("led on!", Point::zero(), text_style, Baseline::Top)
+        Text::with_baseline(get_bme280_result(&mut bme280).as_str(), Point::zero(), text_style, Baseline::Top)
             .draw(&mut display)
             .unwrap();
         led.set_high();
+
         display.flush().unwrap();
         Timer::after_millis(1000).await;
         display.clear_buffer();
         display.flush().unwrap();
 
         info!("led off!");
-        Text::with_baseline("led off!", Point::zero(), text_style, Baseline::Top)
+        Text::with_baseline(get_bme280_result(&mut bme280).as_str(), Point::zero(), text_style, Baseline::Top)
             .draw(&mut display)
             .unwrap();
         led.set_low();
@@ -77,4 +117,3 @@ async fn main(_spawner: Spawner) {
         display.flush().unwrap();
     }
 }
-
